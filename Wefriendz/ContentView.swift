a4d3c2f5a64aec1f497274e5bc2f3934dddc9f1d
chinthaka01 <chinthaka01@gmail.com>
2025-12-22 22:18:10 -0500

    Initial Commit.

---
+++ Wefriendz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wefriendz
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wefriendz/WefriendzApp.swift
@@ -0,0 +1,17 @@
+//
+//  WefriendzApp.swift
+//  Wefriendz
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WefriendzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
